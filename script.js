193a7d9ef9498aad15e9dcdb21532348cf2a4d44
Erikk2402 <57594934+Erikk2402@users.noreply.github.com>
2023-08-31 09:22:38 +0100

    5
    5

--- script.js
+++ script.js
@@ -7,27 +7,67 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19
 }).addTo(map);
 
-// Add event listener for clicking on the map
-map.on('click', function (e) {
-    openCustomMarkerForm(e.latlng);
-});
+// Load map data from the server and create markers
+fetch('server.php')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(location => {
+            L.marker([location.lat, location.lng])
+                .bindPopup(`<b>${location.title}</b><br>${location.description}`)
+                .addTo(map);
+        });
+    });
 
+// Open the custom marker form with title and description
 function openCustomMarkerForm(latlng) {
-    var modal = document.getElementById('marker-form');
-    modal.style.display = 'block';
-
-    var closeButton = document.getElementsByClassName('close')[1];
-    closeButton.onclick = function () {
-        closeModal(modal);
-    };
+    var markerForm = document.getElementById('marker-form');
+    markerForm.style.display = 'block';
 
     var form = document.getElementById('custom-marker-form');
     form.onsubmit = function (event) {
         event.preventDefault();
-        var title = document.getElementById('marker-title').value;
-        var description = document.getElementById('marker-description').value;
+        var title = document.getElementById('custom-marker-title').value;
+        var description = document.getElementById('custom-marker-description').value;
 
-        addCustomMarkerToDatabase(latlng, title, description); // Save data to the database
-        closeModal(modal);
+        addCustomMarkerToDatabase(latlng, title, description);
+        addCustomMarkerToMap(latlng, title, description);
+        closeModal(markerForm);
+    };
+
+    var closeButton = document.getElementById('custom-marker-close');
+    closeButton.onclick = function () {
+        closeModal(markerForm);
     };
 }
+
+// Close the modal
+function closeModal(modal) {
+    modal.style.display = 'none';
+}
+
+// Add a custom marker to the map
+function addCustomMarkerToMap(latlng, title, description) {
+    L.marker(latlng)
+        .bindPopup(`<b>${title}</b><br>${description}`)
+        .addTo(map);
+}
+
+// Add a custom marker to the database
+function addCustomMarkerToDatabase(latlng, title, description) {
+    fetch('server.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `lat=${latlng.lat}&lng=${latlng.lng}&title=${title}&description=${description}`,
+    })
+        .then(response => response.text())
+        .then(data => {
+            console.log(data);
+        });
+}
+
+// Add event listener for clicking on the map
+map.on('click', function (e) {
+    openCustomMarkerForm(e.latlng);
+});
--- server.php
+++ server.php
@@ -4,28 +4,24 @@ $username = "root";
 $password = "admin";
 $dbname = "map_notes_db";
 
-// Connect to the database
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// Check if POST data exists
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
   $lat = $_POST['lat'];
   $lng = $_POST['lng'];
   $title = $_POST['title'];
   $description = $_POST['description'];
 
-  // Insert the new note into the database
-  $sql = "INSERT INTO locations (latitude, longitude, title, description) VALUES ('$lat', '$lng', '$title', '$description')";
+  $sql = "INSERT INTO locations (latitude, longitude, title, description) VALUES ($lat, $lng, '$title', '$description')";
   if ($conn->query($sql) === TRUE) {
-    echo "New note created successfully";
+    echo "New marker created successfully";
   } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
   }
 } else {
-  // Retrieve location data from the database
   $sql = "SELECT * FROM locations";
   $result = $conn->query($sql);
   $locations = array();
@@ -35,12 +31,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         "lat" => $row["latitude"],
         "lng" => $row["longitude"],
         "title" => $row["title"],
-        "description" => $row["description"]
+        "description" => $row["description"] // Include description in the response
       );
     }
   }
 
-  // Return the locations as JSON
   header("Content-type: application/json");
   echo json_encode($locations);
 }
